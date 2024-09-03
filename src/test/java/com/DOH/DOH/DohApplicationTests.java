ddbde22ff2b2ad5de3df60f09d4b76164f53d677
DohoonHan <hsm3809@naver.com>
2024-09-03 10:48:08 +0900

    프로젝트 첫 생성

---
+++ src/test/java/com/DOH/DOH/DohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DOH.DOH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
