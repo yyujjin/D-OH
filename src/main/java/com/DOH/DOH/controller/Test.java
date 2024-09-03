738fed9fde33c960f22a4c277b3ebb85ba13380e
DohoonHan <hsm3809@naver.com>
2024-09-03 11:17:17 +0900

    test html 작성하고 연결 테스트

---
+++ src/main/java/com/DOH/DOH/controller/Test.java
@@ -0,0 +1,12 @@
+package com.DOH.DOH.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+}
