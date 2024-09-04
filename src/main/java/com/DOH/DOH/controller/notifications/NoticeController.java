8557026037943707b394895cbc2bee826b669619
sd8599 <sd8599@naver.com>
2024-09-04 20:52:15 +0900

    Update NoticeController.java
    수정

--- src/main/java/com/DOH/DOH/controller/notifications/NoticeController.java
+++ src/main/java/com/DOH/DOH/controller/notifications/NoticeController.java
@@ -1,7 +1,6 @@
 package com.DOH.DOH.controller.notifications;
 
 import com.DOH.DOH.service.notifications.NoticeService;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,12 +16,12 @@ public class NoticeController {
     @Autowired
     NoticeService noticeService;
 
-    @GetMapping("list")
+    @GetMapping("/list")
     public String noticeList(Model model, @RequestParam(required = false, defaultValue = "1") int page) {
         // 공지사항 목록을 서비스 계층에서 불러와 모델에 추가
         noticeService.getNoticeList(page, model);
 
         // 공지사항 목록 페이지로 이동 (로그인 여부와 상관없이)
-        return "notice/list";
+        return "notifications/List";
     }
 }
