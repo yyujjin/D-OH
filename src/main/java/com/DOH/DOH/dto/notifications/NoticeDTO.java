952ae0216695491a28a720d32810d336d1194da0
sd8599 <sd8599@naver.com>
2024-09-04 20:01:19 +0900

    DTO추가
    DTO추가

--- src/main/java/com/DOH/DOH/dto/notifications/NoticeDTO.java
+++ src/main/java/com/DOH/DOH/dto/notifications/NoticeDTO.java
@@ -1,4 +1,19 @@
 package com.DOH.DOH.dto.notifications;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NoticeDTO {
+    private int noticeNum;        // 공지사항 번호 (Primary Key)
+    private String noticeTitle;   // 공지사항 제목
+    private String noticeContent; // 공지사항 내용
+    private Date noticeCreateTime; // 공지사항 작성 시간
+    private int userNum;          // 작성자 번호 (Foreign Key)
+    private boolean noticeTempSave; // 임시 저장 여부
 }
