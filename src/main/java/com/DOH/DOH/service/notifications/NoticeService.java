82fa5c98861c56c7d670d5f5e044cf82736322d2
sd8599 <sd8599@naver.com>
2024-09-04 20:11:04 +0900

    NoticeService.java 추가
    NoticeService.java 추가

--- src/main/java/com/DOH/DOH/service/notifications/NoticeService.java
+++ src/main/java/com/DOH/DOH/service/notifications/NoticeService.java
@@ -1,4 +1,23 @@
 package com.DOH.DOH.service.notifications;
 
+import org.springframework.ui.Model;
+import com.DOH.DOH.domain.Notice;
+import java.util.List;
+
 public interface NoticeService {
+
+    // 공지사항 목록 조회 (페이징 처리)
+    void getNoticeList(int page, Model model);
+
+    // 공지사항 상세 조회
+//    Notice getNoticeDetail(int noticeNum);
+
+    // 공지사항 작성
+    void writeNotice(Notice notice);
+
+    // 공지사항 수정
+    void updateNotice(Notice notice);
+
+    // 공지사항 삭제
+    void deleteNotice(int noticeNum);
 }
