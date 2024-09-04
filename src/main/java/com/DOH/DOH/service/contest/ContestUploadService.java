e24e7d7dd54b3a4c61474a4df0baa40aac586a49
DarWook <m4768@naver.com>
2024-09-04 17:52:17 +0900

    DB 에 넣어야 하는 값 추가

--- src/main/java/com/DOH/DOH/service/contest/ContestUploadService.java
+++ src/main/java/com/DOH/DOH/service/contest/ContestUploadService.java
@@ -3,6 +3,6 @@ package com.DOH.DOH.service.contest;
 import com.DOH.DOH.dto.contest.ContestUploadDTO;
 
 public interface ContestUploadService {
-    //기업 정보 가져오기
+
     public void saveContest(ContestUploadDTO contestUploadDTO);
 }
