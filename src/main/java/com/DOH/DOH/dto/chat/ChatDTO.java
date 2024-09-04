4cb60e6076eeaede55593bd92a790e201ffeb36c
yyujjin <aba1740@naver.com>
2024-09-04 17:14:14 +0900

    :sparkles: ChatDTO 생성

--- src/main/java/com/DOH/DOH/dto/chat/ChatDTO.java
+++ src/main/java/com/DOH/DOH/dto/chat/ChatDTO.java
@@ -1,4 +1,13 @@
 package com.DOH.DOH.dto.chat;
 
 public class ChatDTO {
+
+    public enum MessageType{
+        ENTER, TALK
+    }
+
+    private MessageType type; //메시지 타입
+    private String rooId; //방 번호
+    private String sender; //메시지 보낸 사람
+    private String message; //메시지
 }
