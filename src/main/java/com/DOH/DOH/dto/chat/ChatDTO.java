794eadf751db848f8113ab6f8c4c551134a41e1f
yyujjin <aba1740@naver.com>
2024-09-04 17:16:52 +0900

    Revert ":sparkles: ChatDTO 생성"
    This reverts commit 4cb60e6076eeaede55593bd92a790e201ffeb36c.

--- src/main/java/com/DOH/DOH/dto/chat/ChatDTO.java
+++ src/main/java/com/DOH/DOH/dto/chat/ChatDTO.java
@@ -1,13 +1,4 @@
 package com.DOH.DOH.dto.chat;
 
 public class ChatDTO {
-
-    public enum MessageType{
-        ENTER, TALK
-    }
-
-    private MessageType type; //메시지 타입
-    private String rooId; //방 번호
-    private String sender; //메시지 보낸 사람
-    private String message; //메시지
 }
