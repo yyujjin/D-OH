67c429dec21cd8440d9e567e218f8c332cc9253e
DarWook <m4768@naver.com>
2024-09-04 17:50:55 +0900

    dto 공백 수정

--- src/main/java/com/DOH/DOH/dto/contest/ContestUploadDTO.java
+++ src/main/java/com/DOH/DOH/dto/contest/ContestUploadDTO.java
@@ -15,11 +15,6 @@ public class ContestUploadDTO {
     private String conLogoName; // 로고명
     private String conBriefing; // 브리핑
 
-
-
-
-
-
 //    파일 업로드
 //    private MultipartFile conFilePath; // 경로
 //    private String fileName; // 파일명
@@ -27,12 +22,12 @@ public class ContestUploadDTO {
 
     private String conTitle; // 콘테스트 제목
 
-    private int conFirstPrice; // 1등
-    private int conSecondPrice; // 2등
-    private int conThirdPrice; // 3등
-    private int conFirstPeople; // 1등 수
-    private int conSecondPeople; // 2등 수
-    private int conThirdPeople; // 3등 수
+    private Integer conFirstPrice; // 1등
+    private Integer conSecondPrice; // 2등
+    private Integer conThirdPrice; // 3등
+    private Integer conFirstPeople; // 1등 수
+    private Integer conSecondPeople; // 2등 수
+    private Integer conThirdPeople; // 3등 수
 
     private Date conStartDate; // 콘테스트 시작일
     private Date conEndDate; // 콘테스트 종료일
