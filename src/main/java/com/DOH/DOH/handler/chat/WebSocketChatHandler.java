4d12412b4641b0b9970b847081f3170e8b19124e
yyujjin <aba1740@naver.com>
2024-09-05 13:01:22 +0900

    :white_check_mark: 웹소켓 핸들러 수정

--- src/main/java/com/DOH/DOH/handler/chat/WebSocketChatHandler.java
+++ src/main/java/com/DOH/DOH/handler/chat/WebSocketChatHandler.java
@@ -1,24 +1,30 @@
 package com.DOH.DOH.handler.chat;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.DOH.DOH.dto.chat.ChatMessageDTO;
+import com.DOH.DOH.dto.chat.ChatRoomDTO;
+import com.DOH.DOH.service.chat.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-
-
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebSocketChatHandler extends TextWebSocketHandler{
 
-    private static final Logger log = LoggerFactory.getLogger(WebSocketChatHandler.class);
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}",payload);
-        TextMessage textMessage = new TextMessage("Welcom welcom!!!!!");
-        session.sendMessage(textMessage);
+        ChatMessageDTO chatMessageDTO = objectMapper.readValue(payload,ChatMessageDTO.class);
+        ChatRoomDTO chatRoomDTO = chatService.findRoomById(chatMessageDTO.getRoomId());
+        chatRoomDTO.handleActions(session,chatMessageDTO,chatService);
     }
 }
