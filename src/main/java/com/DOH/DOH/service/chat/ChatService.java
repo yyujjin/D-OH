5b7d71088e1f63b284174d5914fa22fa2f416b3f
yyujjin <aba1740@naver.com>
2024-09-05 10:34:54 +0900

    :sparkles: 채팅 서비스 구현

--- src/main/java/com/DOH/DOH/service/chat/ChatService.java
+++ src/main/java/com/DOH/DOH/service/chat/ChatService.java
@@ -1,4 +1,48 @@
 package com.DOH.DOH.service.chat;
 
-public interface ChatService {
+import com.DOH.DOH.dto.chat.ChatRoomDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoomDTO> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoomDTO> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    public ChatRoomDTO createRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        ChatRoomDTO chatRoomDTO = ChatRoomDTO.builder()
+                .roomId(randomId)
+                .name(name)
+                .build();
+        chatRooms.put(randomId, chatRoomDTO);
+        return chatRoomDTO;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage((objectMapper.writeValueAsString(message))));
+        }catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 }
