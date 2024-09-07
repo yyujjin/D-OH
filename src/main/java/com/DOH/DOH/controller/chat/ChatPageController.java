d707a5fa6f675f05f86afc0323b527b8f59f708a
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-07 20:22:29 +0900

    :sparkles: ChatPageController 추가

---
+++ src/main/java/com/DOH/DOH/controller/chat/ChatPageController.java
@@ -0,0 +1,15 @@
+package com.DOH.DOH.controller.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatPageController {
+
+    @GetMapping("/start")
+    public String startChatPage(){
+        return "/chat/start-chat";
+    }
+}
