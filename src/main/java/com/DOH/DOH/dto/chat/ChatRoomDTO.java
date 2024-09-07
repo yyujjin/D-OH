6d65cc705e22f87d5dc81284297f74db272f9e1c
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-07 18:20:24 +0900

    :white_check_mark: 채팅방 DTO 수정
     participants 필드 제거 및 client/creator 필드 추가

--- src/main/java/com/DOH/DOH/dto/chat/ChatRoomDTO.java
+++ src/main/java/com/DOH/DOH/dto/chat/ChatRoomDTO.java
@@ -3,13 +3,10 @@ package com.DOH.DOH.dto.chat;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class ChatRoomDTO {
     private String roomId; //채팅방 ID
-    private String roomName; //채팅방 이름
-    private List<String> participants; //채팅방에 참여하는 사용자 리스트
-
+    private String client; //의뢰인
+    private String creator; //제작자
 }
