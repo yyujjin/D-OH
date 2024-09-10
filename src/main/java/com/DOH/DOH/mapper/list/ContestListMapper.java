0ce99dd3b241bce584153bfe431402cb12b36ed5
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-10 14:26:01 +0900

    콘테스트 목록 페이지 DB 값 연결
    콘테스트 목록 페이지 DB 값 연결

--- src/main/java/com/DOH/DOH/mapper/list/ContestListMapper.java
+++ src/main/java/com/DOH/DOH/mapper/list/ContestListMapper.java
@@ -1,7 +1,11 @@
 package com.DOH.DOH.mapper.list;
 
+import com.DOH.DOH.dto.list.ContestListDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
 
 @Mapper
 public interface ContestListMapper {
+    public ArrayList<ContestListDTO> getContestList();
 }
--- src/main/java/com/DOH/DOH/service/list/ContestListService.java
+++ src/main/java/com/DOH/DOH/service/list/ContestListService.java
@@ -1,4 +1,9 @@
 package com.DOH.DOH.service.list;
 
+import com.DOH.DOH.dto.list.ContestListDTO;
+
+import java.util.ArrayList;
+
 public interface ContestListService {
+    public ArrayList<ContestListDTO> getContestList();
 }
