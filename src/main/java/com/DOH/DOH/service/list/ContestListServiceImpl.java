0c0815702264340225ebabc27d15e2f91250fdb4
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-10 14:26:01 +0900

    콘테스트 목록 페이지 DB 값 연결
    콘테스트 목록 페이지 DB 값 연결

--- src/main/java/com/DOH/DOH/service/list/ContestListServiceImpl.java
+++ src/main/java/com/DOH/DOH/service/list/ContestListServiceImpl.java
@@ -1,4 +1,22 @@
 package com.DOH.DOH.service.list;
 
-public class ContestListServiceImpl {
-}
+import com.DOH.DOH.dto.list.ContestListDTO;
+import com.DOH.DOH.mapper.list.ContestListMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service("ContestListService")
+public class ContestListServiceImpl implements ContestListService {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
+    public ArrayList<ContestListDTO> getContestList() {
+        ContestListMapper contestListMapper = sqlSession.getMapper(ContestListMapper.class);
+
+        return contestListMapper.getContestList();
+    }}
