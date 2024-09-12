f36f283ee75022326c7e7f9c5b647fddbb95667b
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-12 12:13:10 +0900

    로그인,회원가입 프론트 파일 업로드 test

--- src/main/java/com/DOH/DOH/controller/user/LoginController.java
+++ src/main/java/com/DOH/DOH/controller/user/LoginController.java
@@ -1,9 +1,49 @@
 package com.DOH.DOH.controller.user;
 
+import com.DOH.DOH.dto.user.LoginDTO;
+import com.DOH.DOH.service.user.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
 
 @Slf4j
 @Controller
 public class LoginController {
+
+    private final LoginService loginService;
+
+    //@Autowired->생성자 주입을 통해 LoginService 주입
+    public LoginController(LoginService loginService) {
+
+        this.loginService = loginService;
+    }
+    //로그인 페이지로 이동
+    @GetMapping("/login")
+    public String showLogin() {
+
+        return "user/login";
+    }
+    //회원가입 페이지로 이동
+    @GetMapping("/register")
+    public String showRegisterForm(){
+
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String showRegister(@ModelAttribute LoginDTO loginDTO) {
+
+        System.out.println(loginDTO);
+
+        loginService.register(loginDTO);
+
+        return "redirect:/login";
+    }
+
 }
