75247afb35ef6514ef4b0a166ba262c2bd349718
sd8599 <sd8599@naver.com>
2024-09-12 16:27:17 +0900

    기본 슬라이드 로직 추가
    기본 슬라이드 로직 추가

--- src/main/resources/static/js/list/main.js
+++ src/main/resources/static/js/list/main.js
@@ -5,14 +5,40 @@
             $('.tabTitle .title').removeClass('active');
             // 클릭된 .title 요소에 active 클래스 추가
             $(this).addClass('active');
-    
+
             // 모든 .tabContent 요소에서 active 클래스 제거
             $('.tabContent').removeClass('active');
             // 클릭된 .title 요소의 인덱스에 해당하는 .tabContent 요소에 active 클래스 추가
             var index = $(this).index();
             $('.tabContent').eq(index).addClass('active');
         });
-
-
     });
-    
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.bannerContent');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
+
+// 자동 재생 기능
+setInterval(nextSlide, 2500); // 5초마다 자동으로 다음 슬라이드로 이동
