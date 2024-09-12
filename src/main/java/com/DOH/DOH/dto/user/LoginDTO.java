75ae30babcf846d96e6692b8efd86ae39171205c
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-12 12:13:57 +0900

    로그인,회원가입 프론트 파일 업로드 test

--- src/main/java/com/DOH/DOH/dto/user/LoginDTO.java
+++ src/main/java/com/DOH/DOH/dto/user/LoginDTO.java
@@ -1,4 +1,13 @@
 package com.DOH.DOH.dto.user;
 
+import lombok.Data;
+
+@Data
 public class LoginDTO {
-}
+        private int userNum;
+        private String userEmail;
+        private String userPassword;
+        //security 권한
+        private String role;
+    }
+
