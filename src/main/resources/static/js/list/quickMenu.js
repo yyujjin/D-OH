2a2c3734d5a629c0724c99a4f7b866ce68a4deec
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-14 15:25:59 +0900

    quickMenu 마크업
    quickMenu 마크업

---
+++ src/main/resources/static/js/list/quickMenu.js
@@ -0,0 +1,19 @@
+
+function chatting() {
+    var chat = $(".chat");
+    var cnt = $("#cnt").val();
+    var ids = cnt == 0 ? 'None':'On';
+    var txt = $("#txt"+ids);
+    if(txt.hasClass("show")){
+        txt.removeClass("show");
+        chat.removeClass("active") 
+    }else{
+        txt.addClass("show");
+        chat.addClass("active");
+    }
+}
+
+function scrollTop(){
+    $(window).scrollTop(0);
+
+}
