e7255536ee5b5c19c4340034b70a0ba0bc2f911e
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-18 00:10:49 +0900

    :sparkles: 메시지 데이터를 동적 HTML 리스트로 출력 (FE)

--- src/main/resources/static/js/list/quickMenu.js
+++ src/main/resources/static/js/list/quickMenu.js
@@ -1,12 +1,21 @@
-var data = [];
+var data = []; //일단 빈 배열로 설정
 
 function chatting() {
   var chat = $(".chat");
-  var cnt = $("#cnt").val();
-  var cnt = data.length === 0 ? 0 : 1;
-  var ids = cnt == 0 ? "None" : "On";
-  var txt = $("#txt" + ids);
 
+  var cnt = $("#cnt").val(); //id가 cnt인 value를 가지고 온다.
+
+  // data가 빈 배열이면 cnt 값을 0으로 설정, 그렇지 않으면 1로 설정
+  var cnt = data.length === 0 ? 0 : 1;
+
+  var ids = cnt == 0 ? "None" : "On";
+
+  //data가 빈배열이 아닐 때
+  if (ids == "On") {
+    updateChatList();
+  }
+
+  var txt = $("#txt" + ids);
   if (txt.hasClass("show")) {
     txt.removeClass("show");
     chat.removeClass("active");
@@ -32,6 +41,7 @@ window.onload = function () {
           success: function (messages) {
             console.log(messages);
             data = messages;
+            //값을 전역변수 data에 저장함
           },
         });
       }, 5000); // 5초마다 새로운 메시지 확인
@@ -44,5 +54,34 @@ function checkLoginStatus() {
   return $.ajax({
     url: "/chat/isLoggedIn",
     method: "GET",
+  });
+}
+
+function updateChatList() {
+  var txt = $("#txtOn"); // 채팅 리스트를 담을 요소
+  txt.html(""); // 기존 내용을 초기화
+
+  Object.keys(data).forEach(function (key) {
+    // key에는 배열의 각 요소가 순서대로 전달됨
+    console.log("현재 key:", key);
+    console.log("현재 key에 해당하는 값:", data[key]);
+
+    var chatHtml = `
+        <div class="wrap">
+          <div class="chatImg"></div>
+          <div class="infoWrap">
+            <div class="content">
+              <div class="name">${key}</div>
+            </div>
+            <div class="info">
+              <div class="introduce">새로운 메시지가 있습니다.</div>
+              <div class="num">
+                <span>${data[key].length}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    txt.append(chatHtml);
   });
 }
