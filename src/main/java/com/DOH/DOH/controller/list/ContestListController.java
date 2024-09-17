3ba8785c68a29ce3acf2da19e3be2dfcf5d8e33d
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-17 22:39:21 +0900

    지원하기 controller 및 css 수정_1차
    지원하기 controller 및 css 수정_1차

--- src/main/java/com/DOH/DOH/controller/list/ContestListController.java
+++ src/main/java/com/DOH/DOH/controller/list/ContestListController.java
@@ -8,18 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
 @Slf4j
 @Controller
+@RequestMapping("/contest/")
 public class ContestListController {
 
     @Autowired
     private ContestListService contestListService;
 
-    @GetMapping("/contest/list")
+    @GetMapping("/list")
     public String contestList(@RequestParam(defaultValue = "1") int page, @RequestParam(name = "orderType", required = false, defaultValue = "current")String orderType, Model model){
 
         PagingDTO dto = new PagingDTO(page,contestListService.getTotalCount(),10);
@@ -32,13 +34,13 @@ public class ContestListController {
         return "list/contestList";
     }
 
-    @GetMapping("/contest/application/terms")
+    @GetMapping("/application/terms")
     public String applictionTerms(){
-        return "list/apply_1";
+        return "list/applicationTerms";
     }
 
-    @GetMapping("/contest/application/write")
+    @GetMapping("/application/write")
     public String applictionWrite(){
-        return "list/apply_2";
+        return "list/applicationWrite";
     }
 }
