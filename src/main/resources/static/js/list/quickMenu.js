d76de1aeeed308605f7a943cc27a31a72e8d807d
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-17 22:45:09 +0900

    :sparkles: 페이지 로드 시 로그인 상태 확인 후 메시지 데이터 가져오는 로직 추가 (FE)

--- src/main/resources/static/js/list/quickMenu.js
+++ src/main/resources/static/js/list/quickMenu.js
@@ -1,19 +1,42 @@
-
 function chatting() {
-    var chat = $(".chat");
-    var cnt = $("#cnt").val();
-    var ids = cnt == 0 ? 'None':'On';
-    var txt = $("#txt"+ids);
-    if(txt.hasClass("show")){
-        txt.removeClass("show");
-        chat.removeClass("active") 
-    }else{
-        txt.addClass("show");
-        chat.addClass("active");
-    }
+  var chat = $(".chat");
+  var cnt = $("#cnt").val();
+  var ids = cnt == 0 ? "None" : "On";
+  var txt = $("#txt" + ids);
+  if (txt.hasClass("show")) {
+    txt.removeClass("show");
+    chat.removeClass("active");
+  } else {
+    txt.addClass("show");
+    chat.addClass("active");
+  }
 }
 
-function scrollTop(){
-    $(window).scrollTop(0);
+function scrollTop() {
+  $(window).scrollTop(0);
+}
+//페이지 로드 될 때마다 로그인 확인
+window.onload = function () {
+  checkLoginStatus().then(function (isLoggedIn) {
+    if (isLoggedIn) {
+      // 로그인이 되어 있을 때만 메시지 확인 로직을 실행
+      setInterval(function () {
+        $.ajax({
+          url: "/chat/messages/check",
+          method: "GET",
+          success: function (data) {
+            console.log(data);
+          },
+        });
+      }, 5000); // 5초마다 새로운 메시지 확인
+    }
+  });
+};
 
+// 로그인 여부를 확인하는 함수 (AJAX로 서버에 요청)
+function checkLoginStatus() {
+  return $.ajax({
+    url: "/chat/isLoggedIn",
+    method: "GET",
+  });
 }
