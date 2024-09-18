23185ec5d674939177fdfa97d701b288e59846c4
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-18 14:06:51 +0900

    :sparkles: sender와 receiver로 메시지 분류하여 Map으로 반환하는 서비스 로직 생성 (BE)

--- src/main/java/com/DOH/DOH/service/chat/MessageService.java
+++ src/main/java/com/DOH/DOH/service/chat/MessageService.java
@@ -3,6 +3,9 @@ package com.DOH.DOH.service.chat;
 import com.DOH.DOH.dto.chat.MessageDTO;
 import com.DOH.DOH.mapper.chat.ChatMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,5 +36,28 @@ public class MessageService {
     //전체 메시지 가져오기
     public List<MessageDTO> allMessages (MessageDTO messageDTO){
         return chatMapper.allMessages(messageDTO);
+    }
+
+    //sender, receiver로 메시지 분류 해서 프론트로 내보내기
+    public Map<String, List<MessageDTO>> filterMessagesBySenderAndReceiver(MessageDTO messageDTO) {
+        List<MessageDTO> senderMessageList = new ArrayList<>();
+        List<MessageDTO> receiverMessageList = new ArrayList<>();
+
+        // 메시지 리스트 순회하면서 sender와 receiver로 구분
+        for (MessageDTO message : allMessages(messageDTO)) {
+            //sender은 자기 자신으로 설정해뒀음 JS에
+            if (message.getSender().equals(messageDTO.getSender())) {
+                senderMessageList.add(message);
+            } else if (message.getReceiver().equals(messageDTO.getReceiver())) {
+                receiverMessageList.add(message);
+            }
+        }
+
+        // 두 리스트를 Map에 담아 반환
+        Map<String, List<MessageDTO>> result = new HashMap<>();
+        result.put("sentMessages", senderMessageList);  // sender 메시지 리스트
+        result.put("receivedMessages", receiverMessageList);  // receiver 메시지 리스트
+
+        return result;
     }
 }
