6fcb79df606df983ae39ae6156905089f5426b5a
sd8599 <167828768+sd8599@users.noreply.github.com>
2024-09-18 17:08:30 +0900

    quickMenu연결
    quickMenu연결(+마이페이지)

--- src/main/resources/static/js/chat/quickMenu.js
+++ src/main/resources/static/js/chat/quickMenu.js
@@ -1,19 +1,17 @@
-
 function chatting() {
     var chat = $(".chat");
     var cnt = $("#cnt").val();
     var ids = cnt == 0 ? 'None':'On';
-    var txt = $("#txt"+ids);
-    if(txt.hasClass("show")){
+    var txt = $("#txt" + ids);
+    if (txt.hasClass("show")) {
         txt.removeClass("show");
-        chat.removeClass("active") 
-    }else{
+        chat.removeClass("active");
+    } else {
         txt.addClass("show");
         chat.addClass("active");
     }
 }
 
-function scrollTop(){
-    $(window).scrollTop(0);
-
+function scrollTop() {
+    $(window).animate({ scrollTop: 0 }, 'slow'); // 올바른 선택자 사용
 }
