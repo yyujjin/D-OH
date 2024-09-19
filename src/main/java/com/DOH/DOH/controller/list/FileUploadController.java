040e9fe25039a00b449baceaef491c49830163ff
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-19 17:15:21 +0900

    S3를 이용하여 이미지 출력 및 textarea focus 기능 추가
    S3를 이용하여 이미지 출력 및 textarea focus 기능 추가

---
+++ src/main/java/com/DOH/DOH/controller/list/FileUploadController.java
@@ -0,0 +1,29 @@
+package com.DOH.DOH.controller.list;
+import com.DOH.DOH.service.list.S3Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+
+    private final S3Service s3Service;
+
+    public FileUploadController(S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = s3Service.uploadFile(file);
+            return ResponseEntity.ok("File uploaded successfully: " + fileUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload failed.");
+        }
+    }
+
+}
