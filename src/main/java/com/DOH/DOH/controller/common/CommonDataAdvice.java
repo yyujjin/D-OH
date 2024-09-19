b8f9458255ccd3b0a95f43f53e741722bf3ff4c3
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-19 17:46:30 +0900

    공통 데이터 로직 리팩토링
    - CommonDataAdvice에서 직접 SecurityContextHolder를 사용하는 대신 UserSessionService로 대체
    - 사용자 이메일, 권한, 닉네임을 UserSessionService를 통해 가져오도록 리팩토링
    - 코드 가독성 및 유지보수성 향상

--- src/main/java/com/DOH/DOH/controller/common/CommonDataAdvice.java
+++ src/main/java/com/DOH/DOH/controller/common/CommonDataAdvice.java
@@ -1,6 +1,7 @@
 package com.DOH.DOH.controller.common;
 
 import com.DOH.DOH.dto.user.CustomUserDetails;
+import com.DOH.DOH.service.user.UserSessionService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
@@ -12,28 +13,17 @@ import java.security.Principal;
 @ControllerAdvice
 public class CommonDataAdvice {
 
-    // 공통적으로 사용되는 데이터를 설정하는 메서드
+    private final UserSessionService userSessionService;
+
+    public CommonDataAdvice(UserSessionService userSessionService) {
+        this.userSessionService = userSessionService;
+    }
+
     @ModelAttribute
-    public void addCommonAttributes(Model model, Principal principal) {
-        String userEmail = principal != null ? principal.getName() : "anonymousUser";
-        String userRole = "ROLE_ANONYMOUS";
-        String nickName = "";
+    public void addCommonAttributes(Model model) {
 
-        // Spring Security에서 현재 사용자 권한을 가져오기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            userRole = authentication.getAuthorities().iterator().next().getAuthority();
-        }
-        // CustomUserDetails로 캐스팅 가능한지 확인
-        if (authentication.getPrincipal() instanceof CustomUserDetails) {
-            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-            nickName = customUserDetails.getNickName();  // 닉네임 가져오기
-            userEmail = customUserDetails.getUsername(); // 이메일 가져오기
-        }
-
-        // 사용자 이메일과 권한을 모델에 추가
-        model.addAttribute("userEmail", userEmail);
-        model.addAttribute("userRole", userRole);
-        model.addAttribute("nickName", nickName);
+        model.addAttribute("userEmail", userSessionService.userEmail());
+        model.addAttribute("userRole", userSessionService.userRole());
+        model.addAttribute("nickName", userSessionService.nickName());
     }
 }
