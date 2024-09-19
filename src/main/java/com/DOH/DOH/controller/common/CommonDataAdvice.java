471e3ac38d9a70446653254704db547da4c1a611
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-19 16:28:38 +0900

    닉네임 필드 추가 및 admin 경로 수정, 세션 유지 시간,
    - 회원가입 닉네임 입력 필드 추가
    - 관리자의 경로를 "/admin/**"로 수정
    - 세션 유지 시간 추가
    - UserSessionService에서 닉네임과 사용자 정보를 세션을 통해 가져오는 로직 추가
    - 공통 데이터 로직 추가:닉네임의 정보를 가져오는 로직 추가

--- src/main/java/com/DOH/DOH/controller/common/CommonDataAdvice.java
+++ src/main/java/com/DOH/DOH/controller/common/CommonDataAdvice.java
@@ -1,5 +1,6 @@
 package com.DOH.DOH.controller.common;
 
+import com.DOH.DOH.dto.user.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
@@ -16,15 +17,23 @@ public class CommonDataAdvice {
     public void addCommonAttributes(Model model, Principal principal) {
         String userEmail = principal != null ? principal.getName() : "anonymousUser";
         String userRole = "ROLE_ANONYMOUS";
+        String nickName = "";
 
         // Spring Security에서 현재 사용자 권한을 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
             userRole = authentication.getAuthorities().iterator().next().getAuthority();
         }
+        // CustomUserDetails로 캐스팅 가능한지 확인
+        if (authentication.getPrincipal() instanceof CustomUserDetails) {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            nickName = customUserDetails.getNickName();  // 닉네임 가져오기
+            userEmail = customUserDetails.getUsername(); // 이메일 가져오기
+        }
 
         // 사용자 이메일과 권한을 모델에 추가
         model.addAttribute("userEmail", userEmail);
         model.addAttribute("userRole", userRole);
+        model.addAttribute("nickName", nickName);
     }
 }
