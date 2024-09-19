2a7c0b8f69edd2b10c1bbb9de07818714bf3a9c8
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-19 09:40:49 +0900

    css, js 수정
    css, js 수정

--- src/main/resources/static/js/list/applicationWrite.js
+++ src/main/resources/static/js/list/applicationWrite.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
 
-    $(".content.text").click(function () {
-        console.log("click!!");
-        var std ="<textarea name='' id='' placeholder='텍스트를 입력해주세요.'></textarea>";
-        // $(".contArea").appand(std); 오타
-        $(".textArea").append(std);
-    });//end of click function
+    function addText(){
+        var std ="<textarea name='' id='' placeholder='텍스트를 입력해주세요.' onkeydown='resize(this)' onkeyup='resize(this)'></textarea>";
 
-    
+        if ($(".textArea").find('textarea').length > 0) {
+            console.log("Textarea 태그가 이미 있음...");
+        } else {
+            console.log("Textarea 태그가 없음!");
+            $(".textArea").append(std);
+        } 
+    }
 
-});//end of document ready function
+
+    function resize(obj) {
+        obj.style.height = '1px';
+        obj.style.height = (12 + obj.scrollHeight) + 'px';
+    }
