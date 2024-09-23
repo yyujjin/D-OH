acbbe66a1907338c0992bd00021835acd558010b
sd8599 <sd8599@naver.com>
2024-09-23 15:19:47 +0900

    Update eventView.js
    수정

--- src/main/resources/static/js/notifications/eventView.js
+++ src/main/resources/static/js/notifications/eventView.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $('.register-btn a').on('click', function (e) {
         e.preventDefault();
@@ -6,15 +5,13 @@ $(document).ready(function () {
     });
 });
 
-    document.querySelector('form').addEventListener('submit', function() {
-        var date = document.getElementById('eventCreateTime').value; // YYYY-MM-DD
-        var time = document.getElementById('eventTime').value; // HH:MM
-        var dateTime = date + 'T' + time; // ISO 8601 형식: YYYY-MM-DDTHH:MM
+document.querySelector('form').addEventListener('submit', function() {
+    var date = document.getElementById('eventCreateTime').value; // YYYY-MM-DD format
 
-        // 새로운 hidden input을 추가하여 결합된 값을 전송
-        var input = document.createElement('input');
-        input.type = 'hidden';
-        input.name = 'eventCreateTime'; // DTO의 필드와 일치해야 함
-        input.value = dateTime;
-        this.appendChild(input);
-    });
+    // Remove time and only send date value
+    var input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = 'eventCreateTime'; // DTO field should match this
+    input.value = date; // Only the date value is sent now
+    this.appendChild(input);
+});
