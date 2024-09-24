7b4e917752b95307d4bac8e7633766e09add32e2
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-25 05:26:21 +0900

    Main 페이지에 값 출력 구현단계 1
    Main 페이지에 값 출력 구현단계 1

--- src/main/java/com/DOH/DOH/controller/list/MainController.java
+++ src/main/java/com/DOH/DOH/controller/list/MainController.java
@@ -1,7 +1,10 @@
 package com.DOH.DOH.controller.list;
 
+import com.DOH.DOH.dto.list.ContestListDTO;
+import com.DOH.DOH.service.list.MainService;
 import com.DOH.DOH.service.user.UserSessionService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,10 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
 public class MainController {
+
+    @Autowired
+    private MainService mainService;
 
     private final UserSessionService userSessionService;
 
@@ -31,6 +38,9 @@ public class MainController {
         log.info("userEmail : {}", userEmail);
         log.info("userRole : {}", userRole);
         log.info("userNickName : {}",userNickName);
+
+        ArrayList<ContestListDTO> imageList = mainService.getImageList();
+        model.addAttribute("imageList",imageList);
 
         return "list/main"; // 해당 템플릿 파일로 리턴
     }
--- src/main/java/com/DOH/DOH/mapper/list/MainMapper.java
+++ src/main/java/com/DOH/DOH/mapper/list/MainMapper.java
@@ -1,7 +1,14 @@
 package com.DOH.DOH.mapper.list;
 
+import com.DOH.DOH.dto.list.ContestListDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
 
 @Mapper
 public interface MainMapper {
+    ArrayList<ContestListDTO> getImageList();
+    ArrayList<ContestListDTO> getLatestList();
+    ArrayList<ContestListDTO> getPriceList();
+    ArrayList<ContestListDTO> getHitList();
 }
