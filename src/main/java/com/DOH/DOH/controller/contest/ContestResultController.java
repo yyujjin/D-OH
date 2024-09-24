68de597e3160b089531d245e613e2ee0f70dfb03
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-25 04:11:14 +0900

    :lipstick: 지원자 리스트 객체 생성 및 지원자 유무에 따른 출력 기능 구현

--- src/main/java/com/DOH/DOH/controller/contest/ContestResultController.java
+++ src/main/java/com/DOH/DOH/controller/contest/ContestResultController.java
@@ -1,19 +1,34 @@
 package com.DOH.DOH.controller.contest;
 
+import com.DOH.DOH.service.contest.ContestUploadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
 public class ContestResultController {
+
+    private final ContestUploadService contestUploadService;
+
+    public ContestResultController(ContestUploadService contestUploadService) {
+        this.contestUploadService = contestUploadService;
+    }
+
+
     @GetMapping("/result")
     public String test() {
         return "/contest/result";
     }
 
     @GetMapping("/contest/award")
-    public String contestAward() {
-        return "/contest/contestAward";
+    public String contestAward(@RequestParam Long contestNum, Model model) {
+
+        model.addAttribute("applicantList",contestUploadService.getContestApplicants(contestNum));
+        return "contest/contestAward";
     }
+
+
 }
