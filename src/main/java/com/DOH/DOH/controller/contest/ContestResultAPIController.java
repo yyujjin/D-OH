6159979dfd87b138abbff1313d8c2c84ca5d4d1d
yyujjin <aba1740@naver.com>
2024-09-25 10:57:26 +0900

    :sparkles: 컨테스트 결과 저장 controller 연결 완료

--- src/main/java/com/DOH/DOH/controller/contest/ContestResultAPIController.java
+++ src/main/java/com/DOH/DOH/controller/contest/ContestResultAPIController.java
@@ -1,5 +1,6 @@
 package com.DOH.DOH.controller.contest;
 
+import com.DOH.DOH.service.contest.ContestAwardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,21 @@ import java.util.Map;
 @RestController
 @Slf4j
 public class ContestResultAPIController {
+
+    private final ContestAwardService contestAwardService;
+
+    public ContestResultAPIController(ContestAwardService contestAwardService) {
+        this.contestAwardService = contestAwardService;
+    }
+
     @PostMapping("/award")
     public ResponseEntity submitAwardResults(@RequestBody Map<String, String> formData ) {
         //유저 이메일이나 컨테스트 id 받아서 들어온 상금 이랑 설정한 상금 인원 같아야 제출가능
         //{user1=none, user2=none}
 
+        contestAwardService.saveAwardResult(formData);
         log.info("요청 : {}",formData);
-        return ResponseEntity.ok("Dd");
+
+        return ResponseEntity.ok().build();
     }
 }
