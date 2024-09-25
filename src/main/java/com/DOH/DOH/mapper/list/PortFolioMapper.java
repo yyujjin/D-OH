8a09c4d5264f2b70339a73b38c9cd01aba8825a1
sooyoonhee <167828911+sooyoonhee@users.noreply.github.com>
2024-09-25 19:10:56 +0900

    포토폴리오 상세 페이지 정보 출력
    포토폴리오 상세 페이지 정보 출력

--- src/main/java/com/DOH/DOH/mapper/list/PortFolioMapper.java
+++ src/main/java/com/DOH/DOH/mapper/list/PortFolioMapper.java
@@ -11,4 +11,5 @@ public interface PortFolioMapper {
     ArrayList<PortFolioDTO> getPortFolioList(@Param("offset") int offset, @Param("pageSize") int pageSize);
     void hitUp(int id);//조회수 증가 메서드
     int getTotalCount();//전체 포토폴리오 조회 메서드
+    PortFolioDTO getPortfolioInfo(int portfolioId);
 }
