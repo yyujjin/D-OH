4ed47c2ce89e72c4c482f91f4f6d88bcab3d809e
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-26 11:35:09 +0900

    아이디와 닉네임 중복 검사를 위한 RegisterMapper 및 SQL 쿼리 작성
    - 회원가입 시 아이디(이메일)와 닉네임 중복 검사를 위한 RegisterMapper 인터페이스 및 XML 쿼리 작성
    - checkUserEmail 메서드를 통해 이메일 중복 여부를 확인하는 쿼리 추가
    - checkNickName 메서드를 통해 닉네임 중복 여부를 확인하는 쿼리 추가

--- src/main/java/com/DOH/DOH/mapper/user/RegisterMapper.java
+++ src/main/java/com/DOH/DOH/mapper/user/RegisterMapper.java
@@ -10,4 +10,10 @@ public interface RegisterMapper {
     //해당 이메일로 조회된 사용자의 정보
     public RegisterDTO getUserByUserEmail(String userEmail);
 
+    //DB 이메일 조회 
+    Integer checkUserEmail(String userEmail);
+
+    //DB 닉네임 조회
+    Integer checkNickName(String nickName);
+
 }
