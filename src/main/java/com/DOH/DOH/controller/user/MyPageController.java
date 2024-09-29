00b55fc4d674e791692acd134b895958071e6171
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-29 15:48:56 +0900

    :sparkles: 마이페이지에서 이미지 저장시 닉네임도 같이 저장되게 구

--- src/main/java/com/DOH/DOH/controller/user/MyPageController.java
+++ src/main/java/com/DOH/DOH/controller/user/MyPageController.java
@@ -85,6 +85,7 @@ public class MyPageController {
         model.addAttribute("profile", profile);
         return "user/MyPage";
     }
+    //여기가 맞는데 dto 에 같이 넣어야함
     @PostMapping("/users/mypage")
     public String postmypage(@RequestParam("profilePhoto") MultipartFile file,Model model, MyPageDTO myPageDTO, MyPageProfileDTO profile) throws IOException {
         //유저 이메일 가져오기
@@ -98,6 +99,7 @@ public class MyPageController {
         if (file != null && !file.isEmpty()) {
             String newFileName = uploadFileToS3Bucket(file);
             myPageDTO.setProfilePhotoPath(newFileName);
+            myPageDTO.setUserNickName(userSessionService.nickName());
         }
         profile.setUserEmail(userEmail);
         myPageService.insertProfilePhoto(myPageDTO);
