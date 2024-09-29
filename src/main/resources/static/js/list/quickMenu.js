e80795c439652e02b3a380eaa3108083c7ff2305
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-09-30 00:13:58 +0900

    :construction: 채팅 아이콘 클릭시 유저 프로필 출력 기능 구현 중

--- src/main/resources/static/js/list/quickMenu.js
+++ src/main/resources/static/js/list/quickMenu.js
@@ -87,7 +87,7 @@ function updateChatList() {
     var chatHtml = `
     <a href="/users/chat?receiver=${key}">
         <div class="wrap">
-          <div class="chatImg"></div>
+          <div class="chatImg"><img src ="${data[key][0].profilePhoto}" ></div>
           <div class="infoWrap">
             <div class="content">
               <div class="name">${key}</div>
